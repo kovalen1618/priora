c02d9e48907f9509cd8697a22f6d3b50759e9aa1
kovalen1618 <kovalen1.618@gmail.com>
2023-04-24 15:27:55 -0500

    No change, splitting off for new approach

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -5,6 +5,14 @@ import './CountdownTimer.css'
 export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComplete }, ref ) {
     // Creating initial state with 60 seconds to work from
     const [time, setTime] = useState(startingMinutes * 60);
+    const [digits, setDigits] = useState({
+        hours0: 0,
+        hours1: 0,
+        minutes0: 0,
+        minutes1: 0,
+        seconds0: 0,
+        seconds1: 0
+    });
 
     const intervalRef = useRef();
 
@@ -20,7 +28,6 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
             clearInterval(intervalRef.current);
             intervalRef.current = setInterval(() => {
                 setTime((prevTime) => prevTime - 1);
-                runTime();
             }, 1000);
         },
         pause: () => clearInterval(intervalRef.current),
@@ -30,6 +37,7 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
     useEffect(() => {
         if (time) {
             setTime(parseInt(time));
+            runTime();
         }
     }, [time])
 
@@ -64,51 +72,46 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
         hours: initElements('hours')
     }
 
-    const runTime = () => {
-        // digits is set to the correct values
-        const hours = Math.floor(time / 3600);
-        const minutes = Math.floor((time % 3600) / 60);
-        const seconds = time % 60;
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
 
-        // Holds correct values
+    const runTime = () => {
+
         const now = {
             h: hours,
             m: minutes,
             s: seconds
         };
 
-        // Holds correct values
+        // Checks if the numer is a double-digit number. If it is, then it gets split into two single-digit numbers. Otherwise, it gets padded with a 0.
         now.h = now.h < 10 ? `0${now.h}` : `${now.h}`;
         now.m = now.m < 10 ? `0${now.m}` : `${now.m}`;
         now.s = now.s < 10 ? `0${now.s}` : `${now.s}`;
 
-        let digits = (({
+        setDigits({
             hours0: parseInt(now.h[0]),
             hours1: parseInt(now.h[1]),
             minutes0: parseInt(now.m[0]),
             minutes1: parseInt(now.m[1]),
             seconds0: parseInt(now.s[0]),
             seconds1: parseInt(now.s[1])
-        }));
-
-        console.log(initializedElements.seconds)
-        
-
+        });
 
         for (const timeMagnitude of Object.keys(initializedElements)) {
             for (const i of ['0', '1']) {
                 // Current contains the current value of the digit
                 const current = parseInt(digits[`${timeMagnitude}${i}`]);
-                let next = 0;
-                if (current > 0) {
-                    next = current - 1;
+                let next = current - 1;
+
+                if (timeMagnitude === 'minutes' || timeMagnitude === 'seconds') {
+                    if (i === '0') next = next <= 5 ? next : 0;
+                    if (i === '1') next = next <= 9 ? next : 0;
                 }
                 
                 
                 const element = initializedElements[timeMagnitude][i];
-                console.log(element)
-                // console.log(element && element.digit)
-
+                
                 if (element && element.digit) {
                     if (!element.digit.dataset.digitBefore) {
                         element.digit.dataset.digitBefore = current;
@@ -116,10 +119,15 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
                         element.digit.dataset.digitAfter = next;
                         element.cardFaceBack.textContent = element.digit.dataset.digitAfter;
                     } else if (element.digit.dataset.digitBefore !== current) {
+
+                        console.log(element.digit.dataset.digitBefore)
                         element.card.addEventListener('transitionend', () => {
                             element.digit.dataset.digitBefore = current;
                             element.cardFaceFront.textContent = element.digit.dataset.digitBefore;
-                            
+                            // TODO: Figure out why all of the digits are being flipped at once instead of only when the time changes
+                            // TODO: Currently, it is flipping all of the digits at once, but it is only changing the text content of the digit that is supposed to change
+                            // TODO: The text content of the other digits is not changing
+                            // TODO: And also, 'next' is setting all the digitAfters to -1 of current instead of just the one that's supposed to decrease
                             const cardClone = element.card.cloneNode(true);
                             cardClone.classList.remove('flipped');
                             element.digit.replaceChild(cardClone, element.card);
@@ -147,14 +155,14 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
             <div className='clock-hours'>
                 <div className='digit digit-left'>
                     <div className='card'>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
                 <div className='digit digit-right'>
-                    <div className='card' onClick={() => {initElements()}}>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                    <div className='card'>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
             </div>
@@ -162,14 +170,14 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
             <div className='clock-minutes'>
                 <div className='digit digit-left'>
                     <div className='card'>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
                 <div className='digit digit-right'>
                     <div className='card'>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
             </div>
@@ -177,17 +185,19 @@ export default forwardRef(function CountdownTimer({ startingMinutes, onTimerComp
             <div className='clock-seconds'>
                 <div className='digit digit-left'>
                     <div className='card'>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
                 <div className='digit digit-right'>
                     <div className='card'>
-                        <div className='card-face card-face-front'>0</div>
-                        <div className='card-face card-face-back'>1</div>
+                        <div className='card-face card-face-front'></div>
+                        <div className='card-face card-face-back'></div>
                     </div>
                 </div>
             </div>
         </div>
     )
 })
+
+
